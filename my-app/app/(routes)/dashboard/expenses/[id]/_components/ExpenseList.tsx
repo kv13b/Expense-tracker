242c0150e57f90985d970508228441c9741b0b4a
karthik <karthikbhatt22@gmail.com>
2025-07-11 06:27:10 +0530

    expense delete in progress

--- my-app/app/(routes)/dashboard/expenses/[id]/_components/ExpenseList.tsx
+++ my-app/app/(routes)/dashboard/expenses/[id]/_components/ExpenseList.tsx
@@ -1,24 +1,57 @@
+import { Trash } from "lucide-react";
+import { getCookie } from "cookies-next/client";
+import { toast } from "sonner";
+
 type ExpenseItem = {
   date: string;
   name: string;
   amount: string;
 };
+const DeleteExpense = async (expense: any) => {
+  const userid = getCookie("userid");
+  console.log(expense.SingleExpenseId);
+  try {
+    await fetch(
+      `/api/expense?userId=${userid}&expenseId=${expense.SingleExpenseId}`
+    );
+  } catch (err) {
+    console.log(err);
+    toast("Coudnt delete the expense");
+  }
+};
 
 const ExpenseList = ({ expenses }: { expenses: ExpenseItem[] }) => {
   return (
     <div className="mt-3">
-      <div className="grid grid-cols-3 bg-slate-200 p-2">
-        <h2>Name</h2>
-        <h2>Amount</h2>
-        <h2>Date</h2>
+      <div className="grid grid-cols-4 bg-slate-200 p-2">
+        <h2 className="font-bold">Name</h2>
+        <h2 className="font-bold">Amount</h2>
+        <h2 className="font-bold">Date</h2>
+        <h2 className="font-bold">Action</h2>
       </div>
-      {expenses.map((expense, index) => (
-        <div key={index} className="grid grid-cols-3 bg-slate-400 p-2">
-          <h2>{expense.name}</h2>
-          <h2>{expense.name}</h2>
-          <h2>{expense.name}</h2>
-        </div>
-      ))}
+      {expenses.map((expense, index) => {
+        const formmatteddate = new Date(expense.date).toLocaleDateString(
+          "en-IN",
+          {
+            year: "numeric",
+            month: "short",
+            day: "numeric",
+          }
+        );
+        return (
+          <div key={index} className="grid grid-cols-4 bg-slate-50 p-2">
+            <h2>{expense.name}</h2>
+            <h2>{expense.amount}</h2>
+            <h2>{formmatteddate}</h2>
+            <h2>
+              <Trash
+                className="text-red-600 cursor-pointer"
+                onClick={() => DeleteExpense(expense)}
+              />
+            </h2>
+          </div>
+        );
+      })}
     </div>
   );
 };
--- my-app/app/api/expense/route.ts
+++ my-app/app/api/expense/route.ts
@@ -1,3 +1,4 @@
+import { error } from "console";
 import { Budget, Expense } from "../../../models/models";
 import { NextRequest, NextResponse } from "next/server";
 import { v4 as uuidv4 } from "uuid";
@@ -22,5 +23,21 @@ export async function GET(req: NextRequest) {
       { error: "Failed to fetch the budgets" },
       { status: 500 }
     );
+  }
+}
+
+export async function DELETE(req: NextRequest) {
+  const userId = req.nextUrl.searchParams.get("userId");
+  const expenseId = req.nextUrl.searchParams.get("expenseId");
+
+  if (!userId || !expenseId) {
+    return NextResponse.json({ error: "Missing parameters" }, { status: 400 });
+  }
+  try {
+    await Expense.delete({ userId, expenseId });
+    return NextResponse.json({ message: "Deleted Successfully" });
+  } catch (err) {
+    console.log(err);
+    return NextResponse.json({ error: "Missing parameters" }, { status: 400 });
   }
 }
