410a09f7e05b8b67dbbb69bd720639a3bd58725f
karthik <karthikbhatt22@gmail.com>
2025-07-26 06:17:49 +0530

    dashboard

--- my-app/app/(routes)/dashboard/page.tsx
+++ my-app/app/(routes)/dashboard/page.tsx
@@ -1,14 +1,32 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { getCookie } from "cookies-next/client";
+import CardInfo from "./expenses/[id]/_components/CardInfo";
 
 function page() {
   const username = getCookie("username");
   console.log("this si usn", username);
+  const [budgetList, setBudgetList] = useState([]);
+  const userid = getCookie("userid");
+
+  useEffect(() => {
+    fetchBudgets();
+  }, [userid]);
+  const fetchBudgets = async () => {
+    try {
+      const res = await fetch(`/api/budget?userId=${userid}`);
+      const json = await res.json();
+      console.log("data ", json.data);
+      if (json.data) setBudgetList(json.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
-    <div className="p-5">
+    <div className="p-8">
       <h2 className="font-bold text-3xl">Hi {username}</h2>
-      <p>Here's What's Happening to Your Money</p>
+      <p className="text-gray-600">Here's What's Happening to Your Money</p>
+      <CardInfo budgetList={budgetList} />
     </div>
   );
 }
