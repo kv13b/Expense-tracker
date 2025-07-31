d7de5e21d0bf628a4db6fffb7c6a707a61b8b573
karthik <karthikbhatt22@gmail.com>
2025-07-31 06:35:58 +0530

    dashboard changes

--- my-app/app/(routes)/dashboard/budgets/_components/BudgetItem.tsx
+++ my-app/app/(routes)/dashboard/budgets/_components/BudgetItem.tsx
@@ -11,34 +11,33 @@ type Budget = {
 function BudgetItem({ budget }: { budget: Budget }) {
   console.log("in bg itme", budget);
   return (
-    <Link
-      href={"/dashboard/expenses/" + budget.expenseId}
-      className="p-5 border rounded-lg hover:shadow-md cursor-pointer h-[170px]"
-    >
-      <div className="flex gap-2 item-center justify-between">
-        <div className="flex gap-2 item-center">
-          <h2 className="text-2xl p-3 px-4 bg-slate-100 rounded-full">
-            {budget.icon}
-          </h2>
-          <div className="font-bold">{budget.name}</div>
+    <Link href={"/dashboard/expenses/" + budget.expenseId}>
+      <div className="p-5 border rounded-lg hover:shadow-md cursor-pointer h-[170px]">
+        <div className="flex gap-2 item-center justify-between">
+          <div className="flex gap-2 item-center">
+            <h2 className="text-2xl p-3 px-4 bg-slate-100 rounded-full">
+              {budget.icon}
+            </h2>
+            <div className="font-bold">{budget.name}</div>
+          </div>
+          <h2 className="font-bold text-primary text-lg">₹{budget.amount}</h2>
         </div>
-        <h2 className="font-bold text-primary text-lg">₹{budget.amount}</h2>
-      </div>
-      <div className="mt-5">
-        <div className="flex items-center justify-between mb-3">
-          <h2 className="text-xs text-slate-400">₹{budget.totalSpent}</h2>
-          <h2 className="text-xs text-slate-400">₹{budget.amount}</h2>
-        </div>
-        <div className="w-full bg-slate-300 h-2 rounded-full">
-          <div
-            className="bg-primary h-2 rounded-full"
-            style={{
-              width: `${Math.min(
-                (Number(budget.totalSpent) / Number(budget.amount)) * 100,
-                100
-              )}%`,
-            }}
-          ></div>
+        <div className="mt-5">
+          <div className="flex items-center justify-between mb-3">
+            <h2 className="text-xs text-slate-400">₹{budget.totalSpent}</h2>
+            <h2 className="text-xs text-slate-400">₹{budget.amount}</h2>
+          </div>
+          <div className="w-full bg-slate-300 h-2 rounded-full">
+            <div
+              className="bg-primary h-2 rounded-full"
+              style={{
+                width: `${Math.min(
+                  (Number(budget.totalSpent) / Number(budget.amount)) * 100,
+                  100
+                )}%`,
+              }}
+            ></div>
+          </div>
         </div>
       </div>
     </Link>
--- my-app/app/(routes)/dashboard/page.tsx
+++ my-app/app/(routes)/dashboard/page.tsx
@@ -4,6 +4,9 @@ import { getCookie } from "cookies-next/client";
 import CardInfo from "./expenses/[id]/_components/CardInfo";
 import BarChart from "./expenses/[id]/_components/BarChartDash";
 import BarChartDash from "./expenses/[id]/_components/BarChartDash";
+import { Budget } from "@/models/models";
+import BudgetList from "./budgets/_components/BudgetList";
+import BudgetItem from "./budgets/_components/BudgetItem";
 
 function page() {
   const username = getCookie("username");
@@ -29,11 +32,16 @@ function page() {
       <h2 className="font-bold text-3xl">Hi {username}</h2>
       <p className="text-gray-600">Here's What's Happening to Your Money</p>
       <CardInfo budgetList={budgetList} />
-      <div className="grid grid-cols-1 md:grid-cols-3 mt-6">
+      <div className="grid grid-cols-1 md:grid-cols-3 mt-6 gap-5">
         <div className="md:col-span-2">
           <BarChartDash budgetList={budgetList} />
         </div>
-        <div>other</div>
+        <div className="gap-2">
+          <h2 className="font-bold text-lg">Latest Budgets</h2>
+          {budgetList.map((budget, index) => (
+            <BudgetItem budget={budget} key={index} />
+          ))}
+        </div>
       </div>
     </div>
   );
